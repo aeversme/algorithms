ea6c1db763fc740e110572fadea1b06dc8a3f350
Alex <aeversme@gmail.com>
2021-11-06 11:55:32 -0400

    refactor karatsuba test

--- test/karatsuba_test.py
+++ test/karatsuba_test.py
@@ -1,26 +1,22 @@
 import pytest
 from karatsuba import karatsuba
 
+input_data = [(87, 52), (12340000, 34560000), (8714, 5267)]
 
-def test_karatsuba_small():
-    x = 87
-    y = 52
+
+def test_karatsuba_small_input():
+    x, y = input_data[0]
     product = karatsuba(x, y)
-
     assert product == 4524
 
 
-def test_karatsuba_large():
-    x = 12340000
-    y = 34560000
+def test_karatsuba_large_input():
+    x, y = input_data[1]
     product = karatsuba(x, y)
-
     assert product == 426470400000000
 
 
-def test_karatsuba_uneven():
-    x = 8714
-    y = 5267
+def test_karatsuba_uneven_input():
+    x, y = input_data[2]
     product = karatsuba(x, y)
-
     assert product == 45896638
